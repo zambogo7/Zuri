f7768cc0a3e949f37caf58690e028a7211bacdff
Zedekiah Ambogo <61427381+zambogo7@users.noreply.github.com>
2022-10-26 01:28:15 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <div className='container'>
         <div  id="profile_img" className="profile">
             <img className='profile-img' src="/src/assets/profile.jpg" alt="" srcset="" />
+            <img className='profile-img' src="src/assets/profile.jpg" alt="profile" />
             <h3>Zedekiah Ambogo</h3>
         </div>
 
